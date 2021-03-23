f4e8f0905672c8742a7aff7688b51fe0f5183be7
Loreto Delgado III <loretodelgadoiii@Loretos-MacBook-Pro.local>
2021-03-23 08:37:48 -0500

    added api routes front and back

---
+++ routes/apiRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/api/books", (req, res) => {
+  db.Book.find({})
+    .then(book => res.json(book))
+    .catch(err => res.status(422).end());
+});
+
+router.post("/api/books", (req, res) => {
+  db.Book.insert(req.body)
+    .then(book => res.json(book))
+    .catch(err => res.status(422).end());
+});
+
+// add delete route
+router.post("/api/books/:id", (req, res) => {
+  db.Book.delete(req.params.id)
+    .then(book => res.json(book))
+    .catch(err => res.status(422).end());
+});
+
+module.exports = router;
